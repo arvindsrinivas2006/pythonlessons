00b07613ea6958d3b1fa01086e906694f3834def
Arvind Srinivas <sriniraman@hotmail.com>
2019-08-07 20:08:01 -0700

    added alien class

---
+++ Aliens/Alien.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.locals import *
+import random
+
+SCREENRECT = Rect(0, 0, 640, 480)
+
+class Alien(pygame.sprite.Sprite):
+    speed = 13
+    animation_cycle = 12
+    images = []
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+        self.facing = random.choice((-1, 1)) * self.speed
+        self.frame = 0
+        
+        if self.facing < 0:
+            self.rect.right = SCREENRECT.right
+    
+    def update(self):
+        self.rect.move_ip(self.facing, 0)
+
+        if not SCREENRECT.contains(self.rect):
+            self.facing = -self.facing
+            self.rect.top = self.rect.bottom + 1
+            self.rect = self.rect.clamp(SCREENRECT)
+        
+        self.frame = self.frame + 1
+        self.image = self.images[self.frame//self.animation_cycle % 3]
+            
